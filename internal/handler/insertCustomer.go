a337a5b37caed816ef48b629152723d5de601d37
ramses.ramirez <ramses.ramirez@exodus.com.mx>
2024-04-22 23:51:45 -0600

    Refactor InsertCustomerHandler to perform operations in a goroutine and communicate the result using a channel

--- internal/handler/insertCustomer.go
+++ internal/handler/insertCustomer.go
@@ -11,28 +11,45 @@ import (
 
 // InsertCustomerHandler is a handler that receives a new customer and inserts it into the database
 func InsertCustomerHandler(requestBody *model.Customer) model.HttpResponse {
-	log.Logger.Infoln("InsertCustomerHandler invoked")
-    // Validate the request body
-    if err := validator.Validate.Struct(requestBody); err != nil {
-        return handleErrorResponse("Error validating request body", err, codes.InvalidArgument, validator.GetValidatorErrorMessage(err))
-    }
-    // Hash the password
-    hashedPassword, err := util.HashPassword(requestBody.Password)
-    if err != nil {
-        return handleErrorResponse("Error hashing password", err, codes.Internal, nil)
-    }
-    requestBody.HashedPassword = hashedPassword
+    log.Logger.Infoln("InsertCustomerHandler invoked")
 
-    // Call the repository to insert the new customer
-    newId, err := repository.InsertNewCostumerDB(requestBody)
-    if err != nil {
-        return handleErrorResponse("Error inserting customer", err, codes.Internal, nil)
-    }
+    // Create a channel to communicate the result back
+    resultCh := make(chan model.HttpResponse)
 
-    return model.HttpResponse{
-        StatusCode:   uint32(codes.OK),
-        Data:         newId,
-        HasError:     false,
-        ErrorMessage: "",
-    }
+    // Perform the operations in a goroutine
+    go func() {
+        defer close(resultCh)
+
+        // Validate the request body
+        if err := validator.Validate.Struct(requestBody); err != nil {
+            resultCh <- handleErrorResponse("Error validating request body", err, codes.InvalidArgument, validator.GetValidatorErrorMessage(err))
+            return
+        }
+
+        // Hash the password
+        hashedPassword, err := util.HashPassword(requestBody.Password)
+        if err != nil {
+            resultCh <- handleErrorResponse("Error hashing password", err, codes.Internal, nil)
+            return
+        }
+        requestBody.HashedPassword = hashedPassword
+
+        // Call the repository to insert the new customer
+        newId, err := repository.InsertNewCostumerDB(requestBody)
+        if err != nil {
+            resultCh <- handleErrorResponse("Error inserting customer", err, codes.Internal, nil)
+            return
+        }
+
+        // Send the successful response
+        resultCh <- model.HttpResponse{
+            StatusCode:   uint32(codes.OK),
+            Data:         newId,
+            HasError:     false,
+            ErrorMessage: "",
+        }
+    }()
+
+    // Wait for the result from the goroutine
+    return <-resultCh
 }
