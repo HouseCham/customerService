82fef3a60d88917c3f772aec1c1e46353526abb9
ramses.ramirez <ramses.ramirez@exodus.com.mx>
2024-04-23 15:52:01 -0600

    crud server functions implemented

--- api/core/grpc/customer.server.go
+++ api/core/grpc/customer.server.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/HouseCham/customerService/internal/handler"
 	"github.com/HouseCham/customerService/internal/log"
+	"github.com/HouseCham/customerService/internal/model"
 )
 
 type CustomerGrpcServer struct {
@@ -22,9 +23,66 @@ func (s *CustomerGrpcServer) InsertCustomer(ctx context.Context, request *Insert
 	handlerResponse := handler.InsertCustomerHandler(requestBody)
 
 	// Check if there was an error
-	response := handleResponseErrors(handlerResponse)
+	response := handleInsertResponseErrors(handlerResponse)
 
 	log.Logger.Println("InsertCustomer gRPC endpoint finished")
 
 	return response, nil
+}
+
+// UpdateCustomer is a gRPC endpoint that receives a customer and updates it in the database
+func (s *CustomerGrpcServer) UpdateCustomer(ctx context.Context, request *UpdateCustomerCommand) (*UpdateCustomerResponse, error) {
+	log.Logger.Println("UpdateCustomer gRPC endpoint invoked")
+
+	// Convert gRPC request to model struct
+	requestBody := mapUpdateCustomerRequestToModel(request)
+
+	// Call the handler to update the customer
+	handlerResponse := handler.UpdateCustomerHandler(requestBody)
+
+	// Check if there was an error
+	response := handleUpdateResponseErrors(handlerResponse)
+
+	log.Logger.Println("UpdateCustomer gRPC endpoint finished")
+
+	return response, nil
+}
+
+// DeleteCustomer is a gRPC endpoint that receives a customer ID and deletes it from the database
+func (s *CustomerGrpcServer) DeleteCustomer(ctx context.Context, request *DeleteCustomerCommand) (*DeleteCustomerResponse, error) {
+	log.Logger.Println("DeleteCustomer gRPC endpoint invoked")
+
+	// Call the handler to delete the customer
+	handlerResponse := handler.DeleteCustomerHandler(uint(request.Id))
+
+	log.Logger.Println("DeleteCustomer gRPC endpoint finished")
+	return &DeleteCustomerResponse{
+		HasError:     handlerResponse.HasError,
+		ErrorMessage: handlerResponse.ErrorMessage,
+		StatusCode:   handlerResponse.StatusCode,
+	}, nil
+}
+
+// GetCustomer is a gRPC endpoint that receives a customer ID and returns the customer from the database
+func (s *CustomerGrpcServer) GetCustomer(ctx context.Context, request *GetCustomerQuery) (*GetCustomerResponse, error) {
+	log.Logger.Println("GetCustomer gRPC endpoint invoked")
+
+	// Call the handler to get the customer
+	handlerResponse := handler.GetCustomerByIdHandler(uint(request.Id))
+
+	log.Logger.Println("GetCustomer gRPC endpoint finished")
+
+	return &GetCustomerResponse{
+		HasError:     handlerResponse.HasError,
+		ErrorMessage: handlerResponse.ErrorMessage,
+		StatusCode:   handlerResponse.StatusCode,
+
+		Id: 		uint32(handlerResponse.Data.(*model.Customer).ID),
+		FirstName: 	handlerResponse.Data.(*model.Customer).FirstName,
+		SecondName: handlerResponse.Data.(*model.Customer).SecondName,
+		LastNameP: 	handlerResponse.Data.(*model.Customer).LastNameP,
+		LastNameM: 	handlerResponse.Data.(*model.Customer).LastNameM,
+		PhoneNumber: handlerResponse.Data.(*model.Customer).PhoneNumber,
+		Email: 		handlerResponse.Data.(*model.Customer).Email,
+	}, nil
 }
--- api/core/grpc/server.utils.go
+++ api/core/grpc/server.utils.go
@@ -17,6 +17,17 @@ func mapInsertCustomerRequestToModel(request *InsertCustomerCommand) *model.Cust
 		Password:    request.Password,
 	}
 }
+// Function to map UpdateCustomerCommand to Customer model
+func mapUpdateCustomerRequestToModel(request *UpdateCustomerCommand) *model.Customer {
+	return &model.Customer{
+		FirstName:   request.FirstName,
+		SecondName:  request.SecondName,
+		LastNameP:   request.LastNameP,
+		LastNameM:   request.LastNameM,
+		PhoneNumber: request.PhoneNumber,
+		Email:       request.Email,
+	}
+}
 
 // transformValidatorErrors transforms an array of ValidatorError to an array of ValidationError
 func transformValidatorErrors(validatorErrors []model.ValidatorError) []*ValidationError {
@@ -32,7 +43,7 @@ func transformValidatorErrors(validatorErrors []model.ValidatorError) []*Validat
 }
 
 // InsertCustomerResponse is the response struct for the InsertCustomer gRPC endpoint
-func handleResponseErrors(handlerResponse model.HttpResponse) *InsertCustomerResponse {
+func handleInsertResponseErrors(handlerResponse model.HttpResponse) *InsertCustomerResponse {
 	response := &InsertCustomerResponse{}
 	// Check if there was an error
 	if handlerResponse.HasError {
@@ -47,6 +58,26 @@ func handleResponseErrors(handlerResponse model.HttpResponse) *InsertCustomerRes
 	} else {
 		response.HasError = false
 		response.NewId = uint32(handlerResponse.Data.(uint))
+		response.StatusCode = handlerResponse.StatusCode
+	}
+	return response
+}
+
+// UpdateCustomerResponse is the response struct for the UpdateCustomer gRPC endpoint
+func handleUpdateResponseErrors(handlerResponse model.HttpResponse) *UpdateCustomerResponse {
+	response := &UpdateCustomerResponse{}
+	// Check if there was an error
+	if handlerResponse.HasError {
+		response.HasError = true
+		response.ErrorMessage = handlerResponse.ErrorMessage
+
+		// Handle validation errors
+		if handlerResponse.StatusCode == uint32(codes.InvalidArgument) {
+			response.ErrorMessage = "invalid request body"
+			response.ValidationErrors = transformValidatorErrors(handlerResponse.Data.([]model.ValidatorError))
+		}
+	} else {
+		response.HasError = false
 		response.StatusCode = handlerResponse.StatusCode
 	}
 	return response
